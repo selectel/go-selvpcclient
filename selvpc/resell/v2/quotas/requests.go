1a3be75f0c789736f7ce470ab801cfdc662db021
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-03-30 10:18:18 +0300

    Add Resell v2 quotas get free method
    Implement GetFree method for quotas.

--- selvpc/resell/v2/quotas/requests.go
+++ selvpc/resell/v2/quotas/requests.go
@@ -29,3 +29,24 @@ func GetAll(ctx context.Context, client *selvpc.ServiceClient) ([]*Quota, *selvp
 
 	return result.Quotas, responseResult, nil
 }
+
+// GetFree returns the current amount of resources available to be allocated to projects.
+func GetFree(ctx context.Context, client *selvpc.ServiceClient) ([]*Quota, *selvpc.ResponseResult, error) {
+	url := strings.Join([]string{client.Endpoint, resourceURL, "free"}, "/")
+	responseResult, err := client.DoRequest(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	if responseResult.Err != nil {
+		return nil, responseResult, responseResult.Err
+	}
+
+	// Extract quotas from the response body.
+	var result ResourcesQuotas
+	err = responseResult.ExtractResult(&result)
+	if err != nil {
+		return nil, responseResult, err
+	}
+
+	return result.Quotas, responseResult, nil
+}
