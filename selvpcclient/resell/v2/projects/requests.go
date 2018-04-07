62c13a7f93433e2bf64f0c9ad0c60f0163f459d4
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-04-07 16:35:56 +0300

    Add Resell V2 projects List method
    Add method to list all domain projects.

--- selvpcclient/resell/v2/projects/requests.go
+++ selvpcclient/resell/v2/projects/requests.go
@@ -31,3 +31,26 @@ func Get(ctx context.Context, client *selvpcclient.ServiceClient, id string) (*P
 
 	return result.Project, responseResult, nil
 }
+
+// List gets a list of projects in the current domain.
+func List(ctx context.Context, client *selvpcclient.ServiceClient) ([]*Project, *selvpcclient.ResponseResult, error) {
+	url := strings.Join([]string{client.Endpoint, resourceURL}, "/")
+	responseResult, err := client.DoRequest(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	if responseResult.Err != nil {
+		return nil, responseResult, responseResult.Err
+	}
+
+	// Extract projects from the response body.
+	var result struct {
+		Projects []*Project `json:"projects"`
+	}
+	err = responseResult.ExtractResult(&result)
+	if err != nil {
+		return nil, responseResult, err
+	}
+
+	return result.Projects, responseResult, nil
+}
--- selvpcclient/resell/v2/projects/testing/requests_test.go
+++ selvpcclient/resell/v2/projects/testing/requests_test.go
@@ -55,3 +55,48 @@ func TestGetProjectSingleQuota(t *testing.T) {
 		t.Fatalf("expected %#v, but got %#v", expected, actual)
 	}
 }
+
+func TestListProjects(t *testing.T) {
+	testEnv := testutils.SetupTestEnv()
+	defer testEnv.TearDownTestEnv()
+	testEnv.NewTestResellV2Client()
+	testEnv.Mux.HandleFunc("/resell/v2/projects", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		fmt.Fprintf(w, TestListProjectsResponseRaw)
+	})
+
+	ctx := context.Background()
+	actual, _, err := projects.List(ctx, testEnv.Client)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if actual == nil {
+		t.Fatal("didn't get projects")
+	}
+	if len(actual) != 2 {
+		t.Errorf("expected 2 projects, but got %d", len(actual))
+	}
+}
+
+func TestListProjectsSingle(t *testing.T) {
+	testEnv := testutils.SetupTestEnv()
+	defer testEnv.TearDownTestEnv()
+	testEnv.NewTestResellV2Client()
+	testEnv.Mux.HandleFunc("/resell/v2/projects", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		fmt.Fprintf(w, TestListProjectsResponseSingleRaw)
+	})
+
+	ctx := context.Background()
+	actual, _, err := projects.List(ctx, testEnv.Client)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := TestListProjectsSingleResponse
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("expected %#v, but got %#v", expected, actual)
+	}
+}
