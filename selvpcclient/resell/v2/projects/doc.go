94215d8f8b718a4be7284044c0340785710978f2
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-04-07 17:40:39 +0300

    Add Resell V2 project update method
    Add a method to update a single Resell v2 project. Add tests and a
    documentation example.

--- selvpcclient/resell/v2/projects/doc.go
+++ selvpcclient/resell/v2/projects/doc.go
@@ -52,5 +52,24 @@ Example of creating a single project
     log.Fatal(err)
   }
   fmt.Println(newProject)
+
+Example of updating a single project
+
+  themeColor := "ffffff"
+  logo := "123"
+  themeUpdateOpts := projects.ThemeUpdateOpts{
+    Color: &themeColor,
+    Logo:  &logo,
+  }
+  name := "test001"
+  updateOpts := projects.UpdateOpts{
+    Name:  &name,
+    Theme: &themeUpdateOpts,
+  }
+  updatedProject, _, err := projects.Update(context, resellClient, newProject.ID, updateOpts)
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Println(updatedProject)
 */
 package projects
--- selvpcclient/resell/v2/projects/testing/requests_test.go
+++ selvpcclient/resell/v2/projects/testing/requests_test.go
@@ -146,3 +146,47 @@ func TestCreateProject(t *testing.T) {
 		t.Fatalf("expected %#v, but got %#v", actualResponse, expectedResponse)
 	}
 }
+
+func TestUpdateProject(t *testing.T) {
+	testEnv := testutils.SetupTestEnv()
+	defer testEnv.TearDownTestEnv()
+	testEnv.NewTestResellV2Client()
+	testEnv.Mux.HandleFunc("/resell/v2/projects/f9ede488e5f14bac8962d8c53d0af9f4", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		fmt.Fprintf(w, TestUpdateProjectResponseRaw)
+
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Errorf("unable to read the request body: %v", err)
+		}
+
+		var actualRequest interface{}
+		err = json.Unmarshal(b, &actualRequest)
+		if err != nil {
+			t.Errorf("unable to unmarshal the request body: %v", err)
+		}
+
+		var expectedRequest interface{}
+		err = json.Unmarshal([]byte(TestUpdateProjectOptsRaw), &expectedRequest)
+		if err != nil {
+			t.Errorf("unable to unmarshal expected raw response: %v", err)
+		}
+
+		if !reflect.DeepEqual(actualRequest, expectedRequest) {
+			t.Fatalf("expected %#v create options, but got %#v", expectedRequest, actualRequest)
+		}
+	})
+
+	ctx := context.Background()
+	updateOpts := TestUpdateProjectOpts
+	actualResponse, _, err := projects.Update(ctx, testEnv.Client, "f9ede488e5f14bac8962d8c53d0af9f4", updateOpts)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedResponse := TestUpdateProjectResponse
+
+	if !reflect.DeepEqual(actualResponse, expectedResponse) {
+		t.Fatalf("expected %#v, but got %#v", actualResponse, expectedResponse)
+	}
+}
