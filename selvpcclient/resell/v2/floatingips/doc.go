1f7cd3900e7cd4c3df20148eb818dfec6bb02ce0
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-04-13 18:12:27 +0300

    Add Resell V2 floating ip GET method (#38)
    Add GET method for the floating ip.
    Add Server package that provides common structure for the floatingips
    and subnets package.
    Add doc example and test.

--- selvpcclient/resell/v2/floatingips/doc.go
+++ selvpcclient/resell/v2/floatingips/doc.go
@@ -2,6 +2,14 @@
 Package floatingips provides the ability to retrieve and manage floatingips through
 the Resell v2 API.
 
+Example of getting a single floating ip referenced by its id
+
+  floatingip, _, err := floatingips.Get(context, resellClient, fipID)
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Println(floatingip)
+
 Example of getting all floatingips
 
   allFloatingIPs, _, err := floatingips.List(ctx, resellClient)
--- selvpcclient/resell/v2/floatingips/requests.go
+++ selvpcclient/resell/v2/floatingips/requests.go
@@ -9,6 +9,29 @@ import (
 
 const resourceURL = "floatingips"
 
+// Get returns a single floating ip by its id.
+func Get(ctx context.Context, client *selvpcclient.ServiceClient, id string) (*FloatingIP, *selvpcclient.ResponseResult, error) {
+	url := strings.Join([]string{client.Endpoint, resourceURL, id}, "/")
+	responseResult, err := client.DoRequest(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	if responseResult.Err != nil {
+		return nil, responseResult, responseResult.Err
+	}
+
+	// Extract a floating ip from the response body.
+	var result struct {
+		FloatingIP *FloatingIP `json:"floatingip"`
+	}
+	err = responseResult.ExtractResult(&result)
+	if err != nil {
+		return nil, responseResult, err
+	}
+
+	return result.FloatingIP, responseResult, nil
+}
+
 // List gets a list of floating ips in the current domain.
 func List(ctx context.Context, client *selvpcclient.ServiceClient) ([]*FloatingIP, *selvpcclient.ResponseResult, error) {
 	url := strings.Join([]string{client.Endpoint, resourceURL}, "/")
