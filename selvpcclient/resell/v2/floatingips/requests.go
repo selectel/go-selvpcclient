2b79d903c209589965e84bf4ba6061c3e5883e9b
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-04-16 00:03:23 +0300

    Add Resell V2 API floating ips Create method (#39)
    Add method to create floating ips in the project.
    Add doc example and documentation example.

--- selvpcclient/resell/v2/floatingips/requests.go
+++ selvpcclient/resell/v2/floatingips/requests.go
@@ -1,7 +1,9 @@
 package floatingips
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"strings"
 
 	"github.com/selectel/go-selvpcclient/selvpcclient"
@@ -36,6 +38,35 @@ func Get(ctx context.Context, client *selvpcclient.ServiceClient, id string) (*F
 func List(ctx context.Context, client *selvpcclient.ServiceClient) ([]*FloatingIP, *selvpcclient.ResponseResult, error) {
 	url := strings.Join([]string{client.Endpoint, resourceURL}, "/")
 	responseResult, err := client.DoRequest(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	if responseResult.Err != nil {
+		return nil, responseResult, responseResult.Err
+	}
+
+	// Extract floating ips from the response body.
+	var result struct {
+		FloatingIPs []*FloatingIP `json:"floatingips"`
+	}
+	err = responseResult.ExtractResult(&result)
+	if err != nil {
+		return nil, responseResult, err
+	}
+
+	return result.FloatingIPs, responseResult, nil
+}
+
+// Create requests a creation of the floating ip in the specified project.
+func Create(ctx context.Context, client *selvpcclient.ServiceClient, projectID string, createOpts FloatingIPOpts) ([]*FloatingIP, *selvpcclient.ResponseResult, error) {
+	createFloatingIPOpts := &createOpts
+	requestBody, err := json.Marshal(createFloatingIPOpts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	url := strings.Join([]string{client.Endpoint, resourceURL, "projects", projectID}, "/")
+	responseResult, err := client.DoRequest(ctx, "POST", url, bytes.NewReader(requestBody))
 	if err != nil {
 		return nil, nil, err
 	}
