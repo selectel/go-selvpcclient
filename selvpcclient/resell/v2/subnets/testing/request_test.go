299251f2c0794ffdae922f7d1ba5f824b5cc352c
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-04-16 08:12:26 +0300

    Resell V2 - add subnets List method
    Add List method with tests and doc example.
    Also fix prefixes in testing fixtures.

--- selvpcclient/resell/v2/subnets/testing/request_test.go
+++ selvpcclient/resell/v2/subnets/testing/request_test.go
@@ -36,3 +36,60 @@ func TestGetSubnet(t *testing.T) {
 		t.Fatalf("expected %#v, but got %#v", expected, actual)
 	}
 }
+
+func TestListSubnets(t *testing.T) {
+	testEnv := testutils.SetupTestEnv()
+	defer testEnv.TearDownTestEnv()
+	testEnv.NewTestResellV2Client()
+	testEnv.Mux.HandleFunc("/resell/v2/subnets", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		fmt.Fprintf(w, TestListSubnetsResponseRaw)
+
+		if r.Method != http.MethodGet {
+			t.Fatalf("expected %s method but got %s", http.MethodGet, r.Method)
+		}
+	})
+
+	ctx := context.Background()
+	actual, _, err := subnets.List(ctx, testEnv.Client)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if actual == nil {
+		t.Fatal("didn't get subnets")
+	}
+	actualKind := reflect.TypeOf(actual).Kind()
+	if actualKind != reflect.Slice {
+		t.Errorf("expected slice of pointers to subnets, but got %v", actualKind)
+	}
+	if len(actual) != 2 {
+		t.Errorf("expected 2 subnets, but got %d", len(actual))
+	}
+}
+
+func TestListSubnetsSingle(t *testing.T) {
+	testEnv := testutils.SetupTestEnv()
+	defer testEnv.TearDownTestEnv()
+	testEnv.NewTestResellV2Client()
+	testEnv.Mux.HandleFunc("/resell/v2/subnets", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		fmt.Fprintf(w, TestListSubnetsSingleResponseRaw)
+
+		if r.Method != http.MethodGet {
+			t.Fatalf("expected %s method but got %s", http.MethodGet, r.Method)
+		}
+	})
+
+	ctx := context.Background()
+	actual, _, err := subnets.List(ctx, testEnv.Client)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := TestListSubnetsSingleResponse
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("expected %#v, but got %#v", expected, actual)
+	}
+}
