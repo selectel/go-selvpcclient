8e9f5236b8bd2f5a44a18a599199697f7c18f0de
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-04-17 20:23:20 +0300

    Resell V2 - add licenses Create method (#51)
    Add Create method for the licenses package with test and doc example.

--- selvpcclient/resell/v2/licenses/testing/requests_test.go
+++ selvpcclient/resell/v2/licenses/testing/requests_test.go
@@ -2,7 +2,9 @@ package testing
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"reflect"
 	"testing"
@@ -91,5 +93,53 @@ func TestListLicensesSingle(t *testing.T) {
 
 	if !reflect.DeepEqual(actual, expected) {
 		t.Fatalf("expected %#v, but got %#v", expected, actual)
+	}
+}
+
+func TestCreateLicenses(t *testing.T) {
+	testEnv := testutils.SetupTestEnv()
+	defer testEnv.TearDownTestEnv()
+	testEnv.NewTestResellV2Client()
+	testEnv.Mux.HandleFunc("/resell/v2/licenses/projects/49338ac045f448e294b25d013f890317", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		fmt.Fprintf(w, TestCreateLicenseResponseRaw)
+
+		if r.Method != http.MethodPost {
+			t.Fatalf("expected %s method but got %s", http.MethodPost, r.Method)
+		}
+
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Errorf("unable to read the request body: %v", err)
+		}
+
+		var actualRequest interface{}
+		err = json.Unmarshal(b, &actualRequest)
+		if err != nil {
+			t.Errorf("unable to unmarshal the request body: %v", err)
+		}
+
+		var expectedRequest interface{}
+		err = json.Unmarshal([]byte(TestCreateLicenseOptsRaw), &expectedRequest)
+		if err != nil {
+			t.Errorf("unable to unmarshal expected raw response: %v", err)
+		}
+
+		if !reflect.DeepEqual(actualRequest, expectedRequest) {
+			t.Fatalf("expected %#v create options, but got %#v", expectedRequest, actualRequest)
+		}
+	})
+
+	ctx := context.Background()
+	createOpts := TestCreateLicenseOpts
+	actualResponse, _, err := licenses.Create(ctx, testEnv.Client, "49338ac045f448e294b25d013f890317", createOpts)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedResponse := TestCreateLicenseResponse
+
+	if !reflect.DeepEqual(actualResponse, expectedResponse) {
+		t.Fatalf("expected %#v, but got %#v", actualResponse, expectedResponse)
 	}
 }
