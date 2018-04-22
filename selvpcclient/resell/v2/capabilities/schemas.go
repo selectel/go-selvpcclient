1c77ccbb2d17b14d152bdc62cb883a7146039567
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-04-22 15:58:18 +0300

    Resell V2 - add cababilites package
    Add cababilites package with a single Get method.

---
+++ selvpcclient/resell/v2/capabilities/schemas.go
@@ -0,0 +1,112 @@
+package capabilities
+
+// Capabilities contains possible availability values of different domain resources.
+type Capabilities struct {
+	// Licenses contains licenses information.
+	Licenses []License `json:"licenses"`
+
+	// Logo contains project logo information.
+	Logo Logo `json:"logo"`
+
+	// Regions contains Identity service regions information.
+	Regions []Region `json:"regions"`
+
+	// Resources contains billing resources information.
+	Resources []Resource `json:"resources"`
+
+	// Subnets contains public subnets information.
+	Subnets []Subnet `json:"subnets"`
+
+	// Traffic contains domain traffic information.
+	Traffic Traffic `json:"traffic"`
+}
+
+// License contains single license information.
+type License struct {
+	// Availability contains availability between locations.
+	Availability []string `json:"availability"`
+
+	// Type represents license type.
+	Type string `json:"type"`
+}
+
+// Logo contains project logo information.
+type Logo struct {
+	// MaxSizeBytes represents maximum valid size of the logo.
+	MaxSizeBytes int `json:"max_size_bytes"`
+}
+
+// Region contains information about single Identity region.
+type Region struct {
+	// Description contains a human-readable region description.
+	Description string `json:"description"`
+
+	// IsDefault shows if region is a default Identity region.
+	IsDefault bool `json:"is_default"`
+
+	// Name contains a human-readable region name.
+	Name string `json:"name"`
+
+	// Zones contains information about different region zones.
+	Zones []Zone `json:"zones"`
+}
+
+// Zone contains information about single availability zone.
+type Zone struct {
+	// Description contains a human-readable region description.
+	Description string `json:"description"`
+
+	// Enabled shows if zone is enabled or not.
+	Enabled bool `json:"enabled"`
+
+	// IsDefault shows if zone is a default availability zone.
+	IsDefault bool `json:"is_default"`
+
+	// Name contains a human-readable region name.
+	Name string `json:"name"`
+}
+
+// Resource contains information about single billing resource.
+type Resource struct {
+	// Name contains a human-readable resource name.
+	Name string `json:"name"`
+
+	// Preordered shows if resource is preordered.
+	Preordered bool `json:"preordered"`
+
+	// QuotaScope shows scope of the resource. It can be region, zone or null.
+	QuotaScope string `json:"quota_scope"`
+
+	// Quotable shows if resource is quotable.
+	Quotable bool `json:"quotable"`
+
+	// Unbillable shows if resource is not should be billed.
+	Unbillable bool `json:"unbillable"`
+}
+
+// Subnet contains information about single public subnet.
+type Subnet struct {
+	// Availability contains availability between locations.
+	Availability []string `json:"availability"`
+
+	// Type represents subnet type.
+	Type string `json:"type"`
+
+	// PrefixLength represents subnet prefix length.
+	PrefixLength string `json:"prefix_length"`
+}
+
+// Traffic contains information about domain traffic.
+type Traffic struct {
+	// Granularities represents traffic granularities.
+	Granularities []Granularity `json:"granularities"`
+}
+
+// Granularity contains information about domain traffic granularity.
+type Granularity struct {
+	// Granularity represents granularity in seconds.
+	Granularity int `json:"granularity"`
+
+	// Timespan represents period of time in days.
+	Timespan int `json:"timespan"`
+}
---
+++ selvpcclient/resell/v2/capabilities/testing/request_test.go
@@ -0,0 +1,115 @@
+package testing
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/selectel/go-selvpcclient/selvpcclient/resell/v2/capabilities"
+	"github.com/selectel/go-selvpcclient/selvpcclient/testutils"
+)
+
+func TestGetCapabilities(t *testing.T) {
+	endpointCalled := false
+
+	testEnv := testutils.SetupTestEnv()
+	defer testEnv.TearDownTestEnv()
+	testEnv.NewTestResellV2Client()
+	testutils.HandleReqWithoutBody(testEnv.Mux, "/resell/v2/capabilities",
+		TestGetCapabilitiesRaw, http.MethodGet, http.StatusOK, &endpointCalled, t)
+
+	ctx := context.Background()
+	capabilities, _, err := capabilities.Get(ctx, testEnv.Client)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !endpointCalled {
+		t.Fatal("endpoint wasn't called")
+	}
+	if capabilities == nil {
+		t.Fatal("didn't get capabilities")
+	}
+	if len(capabilities.Licenses) != 2 {
+		t.Errorf("expected 2 licenses, but got %d", len(capabilities.Licenses))
+	}
+	if len(capabilities.Regions) != 3 {
+		t.Errorf("expected 3 regions, but got %d", len(capabilities.Regions))
+	}
+	if len(capabilities.Resources) != 16 {
+		t.Errorf("expected 16 resources, but got %d", len(capabilities.Resources))
+	}
+	if len(capabilities.Subnets) != 1 {
+		t.Errorf("expected 1 subnets, but got %d", len(capabilities.Subnets))
+	}
+	if len(capabilities.Traffic.Granularities) != 3 {
+		t.Errorf("expected 3 traffic granularities, but got %d", len(capabilities.Traffic.Granularities))
+	}
+}
+
+func TestGetCapabilitiesHTTPError(t *testing.T) {
+	endpointCalled := false
+
+	testEnv := testutils.SetupTestEnv()
+	defer testEnv.TearDownTestEnv()
+	testEnv.NewTestResellV2Client()
+	testutils.HandleReqWithoutBody(testEnv.Mux, "/resell/v2/capabilities",
+		TestGetCapabilitiesRaw, http.MethodGet, http.StatusBadGateway, &endpointCalled, t)
+
+	ctx := context.Background()
+	capabilities, httpResponse, err := capabilities.Get(ctx, testEnv.Client)
+
+	if !endpointCalled {
+		t.Fatal("endpoint wasn't called")
+	}
+	if capabilities != nil {
+		t.Fatal("expected no capabilities from the Get method")
+	}
+	if err == nil {
+		t.Fatal("expected error from the Get method")
+	}
+	if httpResponse.StatusCode != http.StatusBadGateway {
+		t.Fatalf("expected %d status in the HTTP response, but got %d",
+			http.StatusBadGateway, httpResponse.StatusCode)
+	}
+}
+
+func TestGetCapabilitiesTimeoutError(t *testing.T) {
+	testEnv := testutils.SetupTestEnv()
+	testEnv.Server.Close()
+	defer testEnv.TearDownTestEnv()
+	testEnv.NewTestResellV2Client()
+
+	ctx := context.Background()
+	capabilities, _, err := capabilities.Get(ctx, testEnv.Client)
+
+	if capabilities != nil {
+		t.Fatal("expected no capabilities from the Get method")
+	}
+	if err == nil {
+		t.Fatal("expected error from the Get method")
+	}
+}
+
+func TestGetCapabilitiesUnmarshalError(t *testing.T) {
+	endpointCalled := false
+
+	testEnv := testutils.SetupTestEnv()
+	defer testEnv.TearDownTestEnv()
+	testEnv.NewTestResellV2Client()
+	testutils.HandleReqWithoutBody(testEnv.Mux, "/resell/v2/capabilities",
+		TestGetCapabilitiesInvalidRaw, http.MethodGet, http.StatusOK, &endpointCalled, t)
+
+	ctx := context.Background()
+	capabilities, _, err := capabilities.Get(ctx, testEnv.Client)
+
+	if !endpointCalled {
+		t.Fatal("endpoint wasn't called")
+	}
+	if capabilities != nil {
+		t.Fatal("expected no capabilities from the Get method")
+	}
+	if err == nil {
+		t.Fatal("expected error from the Get method")
+	}
+}
