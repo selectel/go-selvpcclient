1db6d8438810c6daab6f49f8f3b1a6f49d5ee2cf
Sergei Kolyshkin <kolyshkin.sa@gmail.com>
2019-01-08 19:07:32 +0300

    Resell V2 - add cross-region subnets List method
    Add crossregionsubnets List method and ListOpts structure.
    Provide tests and doc example.
    Also this commit fixes fields order for Get method fixtures.

--- selvpcclient/resell/v2/crossregionsubnets/requests.go
+++ selvpcclient/resell/v2/crossregionsubnets/requests.go
@@ -32,3 +32,35 @@ func Get(ctx context.Context, client *selvpcclient.ServiceClient, id string) (*C
 
 	return result.CrossRegionSubnet, responseResult, nil
 }
+
+// List gets a list of cross-region subnets in the current domain.
+func List(ctx context.Context, client *selvpcclient.ServiceClient, opts ListOpts) ([]*CrossRegionSubnet, *selvpcclient.ResponseResult, error) {
+	url := strings.Join([]string{client.Endpoint, resourceURL}, "/")
+
+	queryParams, err := selvpcclient.BuildQueryParameters(opts)
+	if err != nil {
+		return nil, nil, err
+	}
+	if queryParams != "" {
+		url = strings.Join([]string{url, queryParams}, "?")
+	}
+
+	responseResult, err := client.DoRequest(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	if responseResult.Err != nil {
+		return nil, responseResult, responseResult.Err
+	}
+
+	// Extract cross-region subnets from the response body.
+	var result struct {
+		CrossRegionSubnets []*CrossRegionSubnet `json:"cross_region_subnets"`
+	}
+	err = responseResult.ExtractResult(&result)
+	if err != nil {
+		return nil, responseResult, err
+	}
+
+	return result.CrossRegionSubnets, responseResult, nil
+}
---
+++ selvpcclient/resell/v2/crossregionsubnets/requests_opts.go
@@ -0,0 +1,6 @@
+package crossregionsubnets
+
+// ListOpts represents options for the List request.
+type ListOpts struct {
+	Detailed bool `param:"detailed"`
+}
