48bba7c2e270c6920a94ec63b029ba9883007d70
Andrei Ozerov <andrei.ozerov92@gmail.com>
2020-06-25 13:42:48 +0300

    Scripts: fix gotest.sh to check for failures
    Check if go test command is failed to select a valid exit code.

--- scripts/gotest.sh
+++ scripts/gotest.sh
@@ -2,10 +2,19 @@
 
 echo "==> Running go test and creating a coverage profile..."
 i=0
+failed=0
 for testingpkg in $(go list ./selvpcclient/.../testing); do
   coverpkg=${testingpkg::-8}
   go test -v -covermode count -coverprofile "./${i}.coverprofile" -coverpkg $coverpkg $testingpkg
+  if [ $? -eq 1 ]; then
+     failed+=1
+  fi
   ((i++))
 done
 gocovmerge $(ls ./*.coverprofile) > coverage.out
 rm *.coverprofile
+
+if ((failed>0)); then
+  exit 1
+fi
+exit 0
