0b6770a8eaac9e0663d0ef80f098cde56989fed5
Tatyana Voloshina <38332876+T-Sh@users.noreply.github.com>
2023-04-10 17:52:10 +0300

    Add github actions with tests and linters (#162)
    Co-authored-by: Voloshina Tatyana <voloshina@selectel.ru>

--- selvpcclient/resell/v2/capabilities/doc.go
+++ selvpcclient/resell/v2/capabilities/doc.go
@@ -4,10 +4,10 @@ through the Resell v2 API.
 
 Example of getting domain capabilities
 
-  domainCapabilities, _, err := capabilities.Get(ctx, resellClient)
-  if err != nil {
-    log.Fatal(err)
-	}
-	fmt.Println(domainCapabilities)
+	  domainCapabilities, _, err := capabilities.Get(ctx, resellClient)
+	  if err != nil {
+	    log.Fatal(err)
+		}
+		fmt.Println(domainCapabilities)
 */
 package capabilities
