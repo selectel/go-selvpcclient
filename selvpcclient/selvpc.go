ac846be53d2969855906541ae3b5e9062e454894
Shnayder Anton <41984158+TruePack@users.noreply.github.com>
2023-12-13 19:34:58 +0700

    Bump app version to 3.1.1 (#170)
    Bump app version to 3.1.1

--- selvpcclient/selvpc.go
+++ selvpcclient/selvpc.go
@@ -15,7 +15,7 @@ import (
 
 const (
 	AppName           = "go-selvpcclient"
-	AppVersion        = "3.0.0"
+	AppVersion        = "3.1.1"
 	DefaultAuthRegion = "ru-1"
 	DefaultAuthURL    = "https://cloud.api.selcloud.ru/identity/v3/"
 )
